dc784cfec1c04551043e6f4702e789f2ab4fab7b
caicangcua <36462470+caicangcua@users.noreply.github.com>
2018-03-18 15:00:44 +0700

    efg

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
 //header("content-type: text/javascript");
 header("Access-Control-Allow-Origin: *");
-echo("12");
+
+if ($_POST){
+    // Make a array with the values
+    $vals = array(
+        'ver'     => 12
+    );
+
+    // Now we want to JSON encode these values to send them to $.ajax success.
+    echo json_encode($vals);
+
+    exit; // to make sure you arn't getting nothing else
+
+} else {
+    // so you can access the error message in jQuery
+    echo json_encode(array('ver' => 12));
+    exit;
+}
 
 ?>
